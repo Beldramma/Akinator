f8531cbef9b93940f329eaa4f4864af55a70da3a
ayamqd <39941512+ayamqd@users.noreply.github.com>
2018-06-05 10:27:45 +0200

    Add files via upload

--- fcts_aya.c
+++ fcts_aya.c
@@ -71,7 +71,9 @@ int note_personnage(int* nouveau_reponses, int* perso_reponses) {  /*recuperer l
 
 
 void maj_note_perso(Liste_Perso* liste_perso) {
-
+  Personnage* ptr_cour;
+  int i;
+  
   ptr_cour=liste_perso->tete;
   for (i=1; i<liste_perso->nb_perso; i++) {
     ptr_cour->note_perso=note_personnage(nouveau_reponses, ptr_cour->liste_reponse);
@@ -88,8 +90,8 @@ void maj_note_perso(Liste_Perso* liste_perso) {
 
 
 void pose_question(Liste_Perso liste_perso) {
-
   int indice_question=1, i;
+  
   m=note_question(1);  
   for (i=1; i<nb_qst-1; i++) { 
       if (note_question(i+1)>m){
@@ -99,9 +101,35 @@ void pose_question(Liste_Perso liste_perso) {
   }
   /* afficher la question numéro "indice_question" */
   return;
-
+}
 
 /*-----------------------------------------*/   
 
-   
+ /* void resultats(Personnages personnages) : entrée :  liste chainée personnages     sortie : /  fonctionnement : affiche le résultat en fonction des notes des personnages (compare les notes).   */
     
+void resultats(Liste_Perso liste_perso) {
+  Personnage* ptr_cour;
+  int i;
+  char* s;
+  
+  ptr_cour=liste_perso->tete;
+  note=ptr_cour->note_perso;
+  s=ptr_cour->nom_perso;
+  for (i=1; i<liste_perso->nb_perso-1; i++) {
+    ptr_cour=ptr_cour->perso_suiv;
+    if (note<ptr_cour->note_perso) {
+      note=ptr_cour->note_perso;
+      s=ptr_cour->nom_perso;
+    }
+  }
+  printf("%s", s);
+  return;
+}
+  
+
+
+
+  
+
+
+  
