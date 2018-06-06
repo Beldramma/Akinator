c7554b9b8ff5bcb64529e7d782282fef1366ece1
ayamqd <39941512+ayamqd@users.noreply.github.com>
2018-06-06 08:43:00 +0200

    Update note_question.c

--- note_question.c
+++ note_question.c
@@ -8,10 +8,8 @@ int note_question(int nb_qst, Liste_Perso *liste_perso) {
   /* reponses: tableau avec le nombre de chaque reponse (incrementée selon les reponses des personnages) */  
   ptr_cour=liste_perso->tete;  
   for (i=0; i<nb_perso; i++) {     /*parcourir les personnages*/
-    for (j=0; j<nb_rep; j++) {    /*parcourir les reponses*/
       reponses[ptr_cour->liste_reponses[nb_qst]->chiffre_reponse]++;
       ptr_cour=ptr_cour->suivant;
-    }
   }
   Q=1;
   for (i=0; i<nb_rep; i++) {
@@ -20,3 +18,4 @@ int note_question(int nb_qst, Liste_Perso *liste_perso) {
 
   return Q;
 }
+
