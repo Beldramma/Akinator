e0c820b701e5c104da3cdda8221232a23f31adeb
ayamqd <39941512+ayamqd@users.noreply.github.com>
2018-06-12 09:00:38 +0200

    Update noooooooote.c

--- noooooooote.c
+++ noooooooote.c
@@ -2,7 +2,7 @@ int note_question(int nb_qst, Liste_Perso liste_personnage)
 {    
 
   int i,j, Q;
-  int reponses[nb_question];
+  int reponses[5];
   Personnage* ptr_cour;
 
 	/* initialisation tableau */
@@ -12,17 +12,13 @@ int note_question(int nb_qst, Liste_Perso liste_personnage)
   /* reponses: tableau tel que l'indice de la case est la réponse et le contenu est le nombre d'occurence de la réponses chez les personnages (incrementée selon les reponses des personnages) */  
   ptr_cour=(Personnage*)malloc(sizeof(Personnage));
   ptr_cour=liste_personnage.tete;
-  for(j=1;j<=5;j++)
-  {
-	  while(ptr_cour!=NULL) {     /*parcourir les personnages*/
-  			if(ptr_cour->liste_reponse[nb_qst]==j)
-  			{
-	      		reponses[j]++;
-	      	}
-	      ptr_cour=ptr_cour->perso_suiv;
-	  }
-	  ptr_cour=liste_personnage.tete;
-	}
+  
+  while(ptr_cour!=NULL) {     /*parcourir les personnages*/
+  			
+	reponses[ptr_cour->liste_reponse[nb_qst]]++;
+	ptr_cour=ptr_cour->perso_suiv;
+  }
+	
   Q=1;
   for (i=1; i<6; i++) {
     Q=Q*(reponses[i]+1);
